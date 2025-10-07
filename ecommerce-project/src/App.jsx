dd8a00d7a1618ab792499c147cb1d789875d5f00
juniord-09 <christdofonsou@gmail.com>
2025-10-07 09:45:10 +0100

    Convert home and checkout to async await

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,31 +1,35 @@
 import axios from 'axios';
 import { Route, Routes } from 'react-router';
 import { useEffect, useState } from 'react';
-import { HomePage } from './pages/home/HomePage'
+import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
-import './App.css'
+import './App.css';
 
 function App() {
-    const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    useEffect(()=> {
-    
-        axios.get('/api/cart-items?expand=product') 
-          .then((response)=> {
-            setCart(response.data);
-          });
-    }, []);
+  useEffect(() => {
+    const fetchAppData = async () => {
+      try {
+        const response = await axios.get('/api/cart-items?expand=product');
+        setCart(response.data);
+      } catch (error) {
+        console.error('Erreur lors du chargement du panier :', error);
+        setCart([]); // sécurité en cas d'erreur
+      }
+    };
 
+    fetchAppData();
+  }, []);
 
   return (
     <Routes>
-      <Route index element= {<HomePage cart={cart} />} />
-      <Route path="/checkout" element= {<CheckoutPage cart={cart} />} />
-      <Route path="/orders" element= {<OrdersPage cart={ cart} />} />
+      <Route index element={<HomePage cart={cart} />} />
+      <Route path="/checkout" element={<CheckoutPage cart={cart} />} />
+      <Route path="/orders" element={<OrdersPage cart={cart} />} />
     </Routes>
-    
-  )
+  );
 }
 
-export default App
+export default App;
